ab3814ff71304823a6643319393134c3923068cb
Caine Benoy <cainebenoy@gmail.com>
2025-01-11 14:50:34 +0530

    Final Step commit before checking if all the requirments of the project are met.

--- dcrm/views.py
+++ dcrm/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
